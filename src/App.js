de5d9722d49baefa904bfd574ed72a9cb4d662ae
lee_c <gitleelee@gmail.com>
2023-12-17 21:26:53 -0800

    Clean-up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { facts } from "./facts";
 
-function App() {
+const App = () => {
+  const [index, setIndex] = useState(0);
+  const [showName, setShowName] = useState(false);
+
+  const nextFact = () => {
+    setShowName(false);
+    index === facts.length - 1 ? setIndex(0) : setIndex(index + 1);
+  };
+
+  useEffect(() => {
+    console.log("Running useEffect");
+    const factDelay = setTimeout(() => {
+      console.log("in Timeout function");
+      setShowName(true);
+    }, 2000);
+
+    return () => {
+      clearTimeout(factDelay);
+    };
+  }, [showName]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {facts && (
+        <article>
+          <p>{facts[index].fact}</p>
+          {showName && <h1>{facts[index].name}</h1>}
+          <button className="fetch-fact" onClick={nextFact}>
+            Next fact
+          </button>
+        </article>
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/facts.js
@@ -0,0 +1,50 @@
+export const facts = [
+  {
+    name: "Ada Lovelace",
+    fact: "First computer programmer in history."
+  },
+  {
+    name: "Grace Hopper",
+    fact: 'Invented the first compiler and coined the term "debugging".'
+  },
+  {
+    name: "Radia Perlman",
+    fact:
+      "Invented the Spanning Tree Protocol (STP) that is used in modern computer networks."
+  },
+  {
+    name: "Hedy Lamarr",
+    fact:
+      "Invented a frequency hopping spread spectrum technique that is now used in Wi-Fi, Bluetooth, and other wireless communications."
+  },
+  {
+    name: "Carol Shaw",
+    fact:
+      'First female video game designer who worked on Atari 2600 games such as "River Raid" and "3D Tic-Tac-Toe".'
+  },
+  {
+    name: "Elaine Rich",
+    fact:
+      "Created the first AI system to recognize human emotions in natural language."
+  },
+  {
+    name: "Sister Mary Kenneth Keller",
+    fact:
+      "One of the first women to earn a Ph.D. in Computer Science and helped develop the BASIC programming language."
+  },
+  {
+    name: "Shafi Goldwasser",
+    fact:
+      "Award-winning cryptographer and computer scientist who co-invented the probabilistic encryption scheme."
+  },
+  {
+    name: "Reshma Saujani",
+    fact:
+      "Founder of Girls Who Code, a nonprofit organization that aims to close the gender gap in technology fields."
+  },
+  {
+    name: "Margaret Hamilton",
+    fact:
+      "Lead software engineer for the Apollo space program that sent humans to the moon."
+  }
+];
